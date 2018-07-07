13c20e1cd3ee35f61ea3efd43c390b79ef941ad1
rarblack <bagiyevaydin@gmail.com>
2018-07-07 23:26:12 +0400

    This is the newly created django project and very first commit to check the git integration of pycharm.

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\bagiy\PycharmProjects\LocalFormFillingandAuthorization\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
